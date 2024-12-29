8fd3b49518ec66d2c3540b2888635e3f8b636d11
Amusat Gifty <amusatgifty@gmail.com>
2024-12-28 22:14:10 -0800

    updated task route

--- model/Task.js
+++ model/Task.js
@@ -4,7 +4,7 @@ const taskSchema = new mongoose.Schema({
   title: {type: String, required: true},
   description: {type: String, required: true},
   status: {type: String, required: true},
-  duedate: {type: Date},
+  due_date: {type: Date},
   createdAt: {type: Date, default: Date.now}
 })
 
